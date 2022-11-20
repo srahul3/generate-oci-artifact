0bbdb7dfc43552ffa6588742a10ec2b48806471f
Rahul <srahul3@github.com>
2022-11-20 12:27:48 +0530

    Update docker-api-client.ts

--- src/docker-api-client.ts
+++ src/docker-api-client.ts
@@ -5,6 +5,19 @@ import * as fs from 'fs'
 // Publish an OCI image to the GHCR using docker http v2 specification
 export async function generate(): Promise<void> {
   try {
+    const repository: string = process.env.GITHUB_REPOSITORY || ''
+    if (repository === '') {
+      core.setFailed('Could not find Repository!')
+      return
+    }
+
+    if (github.context.eventName === 'release') {
+      const semver: string = github.context.payload.release.tag_name
+    }
+
+    const layers: string = core.getInput('layers')
+    const annotations: string = core.getInput('annotations')
+    
     const TOKEN: string = core.getInput('token')
     core.setSecret(TOKEN)
   } catch (error) {
