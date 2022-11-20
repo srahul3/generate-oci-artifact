3737cc4bff3fd52532ceb11fc5cca9932b141c18
Rahul <srahul3@github.com>
2022-11-20 12:17:14 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
+import * as docker_api from './docker-api-client'
 
 async function run(): Promise<void> {
   try {
+    const repository: string = process.env.GITHUB_REPOSITORY || ''
+    if (repository === '') {
+      core.setFailed('Could not find Repository!')
+      return
+    }
+
+    if (github.context.eventName === 'release') {
+      const semver: string = github.context.payload.release.tag_name
+    }
+
+    const layers: string = core.getInput('layers')
+    const annotations: string = core.getInput('annotations')
+    
+
+    const response = await docker_api.generate()
+    
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
